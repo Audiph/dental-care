394429e245da0840cd6748c5c574594733b61bca
audiph <lagarde.marcjoseph@gmail.com>
2023-11-17 04:53:18 +0800

    refactor router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,27 @@ import { useMemo } from 'react';
 import { ThemeProvider, CssBaseline, Box } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import { themeSettings } from './theme';
-import { BrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Error, HomeLayout } from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+  },
+]);
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Box width="100%" height="100%"></Box>
-        </ThemeProvider>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box width="100%" height="100%">
+          <RouterProvider router={router} />
+        </Box>
+      </ThemeProvider>
     </div>
   );
 }
