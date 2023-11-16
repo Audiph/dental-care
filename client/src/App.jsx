fdac62bfefd433e65149fe00e98dafeb64a0e314
audiph <lagarde.marcjoseph@gmail.com>
2023-11-17 00:38:27 +0800

    setup base app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,21 @@
+import { useMemo } from 'react';
+import { ThemeProvider, CssBaseline, Box } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
+import { BrowserRouter } from 'react-router-dom';
+
 function App() {
-  return <div className="app"></div>;
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%"></Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
