3138056804741bb2d9bde7357fb67aa7be4dabe8
audiph <lagarde.marcjoseph@gmail.com>
2023-11-18 15:53:11 +0800

    add id

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,13 +1,16 @@
 export const navLinks = [
   {
+    id: 1,
     path: '/',
     name: 'Home',
   },
   {
+    id: 2,
     path: '/appointments',
     name: 'Appointments',
   },
   {
+    id: 3,
     path: '/apply-doctor',
     name: 'Apply as Doctor',
   },
