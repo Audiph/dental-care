79ffb38887c85db4f4576aee6c2b99c3bbe6693b
audiph <lagarde.marcjoseph@gmail.com>
2023-11-20 04:58:07 +0800

    add get all users

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -1,14 +1,50 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { BASE_URL } from '../utils/constants';
+import axios from 'axios';
+import { hideLoading, showLoading } from './alertsSlice';
+
+export const getAllUsers = createAsyncThunk(
+  'user/getAllUsers',
+  async (_, thunkAPI) => {
+    try {
+      thunkAPI.dispatch(showLoading());
+      const res = await axios
+        .get(`${BASE_URL}/api/admin/get-all-users`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        })
+        .then((res) => res)
+        .catch((err) => err.response);
+
+      if (!res.data.success) {
+        dispatch(hideLoading());
+        return;
+      }
+      thunkAPI.dispatch(hideLoading());
+      return res.data.users;
+    } catch (error) {
+      console.error(error);
+      thunkAPI.dispatch(hideLoading());
+      return thunkAPI.rejectWithValue('something went wrong');
+    }
+  }
+);
 
 export const userSlice = createSlice({
   name: 'user',
   initialState: {
+    users: [],
     login: true,
     user: null,
   },
   reducers: {
     setUser: (state, action) => {
       state.user = action.payload;
+    },
+
+    setUsers: (state, action) => {
+      state.users = action.payload;
     },
 
     toggleLogin: (state, action) => {
@@ -23,6 +59,14 @@ export const userSlice = createSlice({
       state.user = null;
     },
   },
+
+  extraReducers: (builder) => {
+    builder.addCase(getAllUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.users = action.payload;
+    });
+  },
 });
 
-export const { setUser, toggleLogin, showRegister, logout } = userSlice.actions;
+export const { setUser, setUsers, toggleLogin, showRegister, logout } =
+  userSlice.actions;
