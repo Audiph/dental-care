b21a6e3d7b2bf302d6f826b359e2d24bc19b10dd
audiph <lagarde.marcjoseph@gmail.com>
2023-11-20 23:50:02 +0800

    remove duplicate fetching get user by id

--- client/src/pages/HomeLayout.jsx
+++ client/src/pages/HomeLayout.jsx
@@ -1,38 +1,11 @@
 import { Fragment } from 'react';
 import { Navbar } from '@/components';
 import { Outlet } from 'react-router-dom';
-import axios from 'axios';
-import { useEffect } from 'react';
-import { BASE_URL } from '../utils/constants';
 import { Loading } from '../components';
 import { useSelector } from 'react-redux';
 
 const HomeLayout = () => {
   const { loading } = useSelector((state) => state.alerts);
-
-  const fetchData = async () => {
-    try {
-      await axios
-        .post(
-          `${BASE_URL}/api/user/get-user-info-by-id`,
-          {},
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem('token')}`,
-            },
-          }
-        )
-        .then((res) => res)
-        .catch((err) => err.response);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
     <Fragment>
       {loading && <Loading />}
