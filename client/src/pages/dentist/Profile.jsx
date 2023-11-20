6bac0e955ccc5685eb0d670eb9c40aa685848643
audiph <lagarde.marcjoseph@gmail.com>
2023-11-21 03:32:29 +0800

    integrate update dentist profile

--- client/src/pages/dentist/Profile.jsx
+++ client/src/pages/dentist/Profile.jsx
@@ -4,15 +4,57 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { Container, useTheme } from '@mui/material';
 import { useEffect } from 'react';
 import { getDentistData } from '../../redux/dentistSlice';
+import { BASE_URL, delimiter } from '../../utils/constants';
+import toast from 'react-hot-toast';
+import { hideLoading, showLoading } from '../../redux/alertsSlice';
+import axios from 'axios';
 
 const Profile = () => {
+  const { user } = useSelector((state) => state.user);
   const { dentist } = useSelector((state) => state.dentist);
   const { palette } = useTheme();
   const params = useParams();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const updateDentist = Object.fromEntries(formData);
+    const newTimings = updateDentist.timings.split(delimiter, 2);
+
+    try {
+      dispatch(showLoading());
+      const res = await axios
+        .post(
+          `${BASE_URL}/api/dentist/update-dentist-profile`,
+          {
+            ...updateDentist,
+            userId: user.id,
+            timings: newTimings,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+          }
+        )
+        .then((res) => res)
+        .catch((err) => err.response);
+
+      if (!res.data.success) {
+        toast.error(res.data.message);
+        dispatch(hideLoading());
+        return;
+      }
+      dispatch(hideLoading());
+      toast.success(res.data.message);
+      navigate('/');
+    } catch (error) {
+      toast.error('Something went wrong!');
+      console.error(error);
+      dispatch(hideLoading());
+    }
   };
 
   useEffect(() => {
