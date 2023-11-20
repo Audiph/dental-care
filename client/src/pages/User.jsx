849471b7267ac03073fd91adb9e29978b20f607c
audiph <lagarde.marcjoseph@gmail.com>
2023-11-20 19:52:34 +0800

    move fetching user by id info to HomeLayout

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -1,38 +1,6 @@
-import axios from 'axios';
-import { useEffect } from 'react';
-import { BASE_URL } from '../utils/constants';
-import { Loading } from '../components';
-import { useSelector } from 'react-redux';
-
 const User = () => {
-  const { loading } = useSelector((state) => state.alerts);
-
-  const fetchData = async () => {
-    try {
-      await axios
-        .post(
-          `${BASE_URL}/api/user/get-user-info-by-id`,
-          {},
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem('token')}`,
-            },
-          }
-        )
-        .then((res) => res)
-        .catch((err) => err.response);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   return (
     <div>
-      {loading && <Loading />}
       <h1>User</h1>
     </div>
   );
