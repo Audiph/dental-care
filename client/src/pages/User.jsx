0bf4312cef7b42e92623343400c79de184ce0149
audiph <lagarde.marcjoseph@gmail.com>
2023-11-20 23:50:34 +0800

    implement base component for user page

--- client/src/pages/User.jsx
+++ client/src/pages/User.jsx
@@ -1,8 +1,27 @@
+import { Container, useTheme } from '@mui/material';
+import { PageHeader } from '../components';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 const User = () => {
+  const { user } = useSelector((state) => state.user);
+  console.log('🚀 ~ file: User.jsx:7 ~ User ~ user:', user);
+  const { palette } = useTheme();
+  const navigate = useNavigate();
+
+  if (user?.isAdmin) {
+    navigate('/admin/users-list');
+    return;
+  }
+
   return (
-    <div>
-      <h1>User</h1>
-    </div>
+    <Container sx={{ position: 'absolute', minWidth: '100%' }}>
+      <PageHeader
+        title="NOTIFICATIONS"
+        color={palette.tertiary[500]}
+        style={{ paddingTop: '57px' }}
+      />
+    </Container>
   );
 };
 
