cd59e9d67bfc8b9783997ada416f2699de831e39
audiph <lagarde.marcjoseph@gmail.com>
2023-11-21 01:11:18 +0800

    create get dentist by info reducer

--- client/src/redux/dentistSlice.js
+++ client/src/redux/dentistSlice.js
@@ -3,6 +3,40 @@ import { BASE_URL } from '../utils/constants';
 import axios from 'axios';
 import { hideLoading, showLoading } from './alertsSlice';
 
+export const getDentistData = createAsyncThunk(
+  'dentist/getDentistData',
+  async (params, thunkAPI) => {
+    try {
+      thunkAPI.dispatch(showLoading());
+      const res = await axios
+        .post(
+          `${BASE_URL}/api/dentist/get-dentist-info-by-user-id`,
+          {
+            userId: params.userId,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+          }
+        )
+        .then((res) => res)
+        .catch((err) => err.response);
+
+      if (!res.data.success) {
+        thunkAPI.dispatch(hideLoading());
+        return;
+      }
+      thunkAPI.dispatch(hideLoading());
+      return res.data.dentist;
+    } catch (error) {
+      console.error(error);
+      thunkAPI.dispatch(hideLoading());
+      return thunkAPI.rejectWithValue('something went wrong');
+    }
+  }
+);
+
 export const getAllDentists = createAsyncThunk(
   'dentist/getAllDentists',
   async (_, thunkAPI) => {
@@ -18,7 +52,7 @@ export const getAllDentists = createAsyncThunk(
         .catch((err) => err.response);
 
       if (!res.data.success) {
-        dispatch(hideLoading());
+        thunkAPI.dispatch(hideLoading());
         return;
       }
       thunkAPI.dispatch(hideLoading());
@@ -35,6 +69,7 @@ export const dentistSlice = createSlice({
   name: 'dentist',
   initialState: {
     dentists: [],
+    dentist: null,
   },
   reducers: {
     setDentists: (state, action) => {
@@ -44,8 +79,10 @@ export const dentistSlice = createSlice({
 
   extraReducers: (builder) => {
     builder.addCase(getAllDentists.fulfilled, (state, action) => {
-      state.isLoading = false;
       state.dentists = action.payload;
+    });
+    builder.addCase(getDentistData.fulfilled, (state, action) => {
+      state.dentist = action.payload;
     });
   },
 });
