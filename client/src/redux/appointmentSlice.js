c25aa5ebae8a3e6ea8216731d3e4638093e1ff1d
audiph <lagarde.marcjoseph@gmail.com>
2023-11-21 18:25:28 +0800

    create appointments reducer

---
+++ client/src/redux/appointmentSlice.js
@@ -0,0 +1,52 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { BASE_URL } from '../utils/constants';
+import axios from 'axios';
+import { hideLoading, showLoading } from './alertsSlice';
+
+export const getAllAppointmentsByUser = createAsyncThunk(
+  'appointment/getAllAppointmentsByUser',
+  async (_, thunkAPI) => {
+    try {
+      thunkAPI.dispatch(showLoading());
+      const res = await axios
+        .get(`${BASE_URL}/api/user/get-appointments-by-user-id`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`,
+          },
+        })
+        .then((res) => res)
+        .catch((err) => err.response);
+
+      if (!res.data.success) {
+        dispatch(hideLoading());
+        return;
+      }
+      thunkAPI.dispatch(hideLoading());
+      return res.data.appointments;
+    } catch (error) {
+      console.error(error);
+      thunkAPI.dispatch(hideLoading());
+      return thunkAPI.rejectWithValue('something went wrong');
+    }
+  }
+);
+
+export const appointmentSlice = createSlice({
+  name: 'appointment',
+  initialState: {
+    appointments: [],
+  },
+  reducers: {
+    setAppointments: (state, action) => {
+      state.appointments = action.payload;
+    },
+  },
+
+  extraReducers: (builder) => {
+    builder.addCase(getAllAppointmentsByUser.fulfilled, (state, action) => {
+      state.appointments = action.payload;
+    });
+  },
+});
+
+export const { setAppointments } = appointmentSlice.actions;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,11 +3,13 @@ import { combineReducers } from '@reduxjs/toolkit';
 import { alertsSlice } from './alertsSlice';
 import { userSlice } from './userSlice';
 import { dentistSlice } from './dentistSlice';
+import { appointmentSlice } from './appointmentSlice';
 
 const rootReducer = combineReducers({
   alerts: alertsSlice.reducer,
   user: userSlice.reducer,
   dentist: dentistSlice.reducer,
+  appointment: appointmentSlice.reducer,
 });
 
 export const store = configureStore({
